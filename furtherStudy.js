61115e46c1eb7e1e42d4580b400f1f3b4d4f9258
sierra hughey <sierrahughey@mailbox.org>
2021-08-03 00:48:42 +0000

    further study

--- furtherStudy.js
+++ furtherStudy.js
@@ -3,6 +3,21 @@
 
 function wordsInCommon(words1, words2) {
   // Replace this with your code
+  const words1Set = new Set(words1);
+  console.log(words1Set)
+  const words2Set = new Set(words2);
+  console.log(words2Set)
+
+  const results = new Set();
+
+  for (const word of words1Set) {
+    if(words2Set.has(word)){
+      results.add(word);
+    }
+  }
+
+  return Array.from(results);
+
 }
 
 
